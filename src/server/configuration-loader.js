c8de94bfbe3f85986776630e4e3b670bd9237a3e
sahil <sahiltrambadiya777@gmail.com>
2022-09-17 15:24:00 +0530

    fix(*): cors

--- src/server/configuration-loader.js
+++ src/server/configuration-loader.js
@@ -1,7 +1,7 @@
 const argv = require("yargs").argv;
 // const { assign, isEmpty } = require("lodash");
 
-const { NODE_ENV = "local", APP_PORT = "5000" } = process.env;
+const { NODE_ENV = "local", APP_PORT = 5000 } = process.env;
 
 const packageInfo = require("../../package.json");
 
